045f06e9d721dc2e5c0069eafc2d1dcb8a62485c
David Steiner <dimstein.nz@gmail.com>
2020-01-09 10:08:19 +1300

    login finish

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context);
+    return StreamBuilder<User>(
+        stream: auth.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            User user = snapshot.data;
+            if (user == null) {
+              return SignInPage.create(context);
+            }
+            return HomePage();
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("this is it"),
+              ),
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
---
+++ lib/common_widgets/platform_alert_dialog.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_widget.dart';
+
+class PlatformAlertDialog extends PlatformWidget {
+  final String title;
+  final String content;
+  final String defaultActionText;
+  final String cancelActionText;
+
+  PlatformAlertDialog(
+      {@required this.title,
+      @required this.content,
+      @required this.defaultActionText,
+      this.cancelActionText})
+      : assert(title != null),
+        assert(content != null),
+        assert(defaultActionText != null);
+
+  Future<bool> show(BuildContext context) async {
+    return Platform.isIOS
+        ? await showCupertinoDialog<bool>(
+            context: context, builder: (context) => this)
+        : await showDialog<bool>(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => this,
+          );
+  }
+
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: _buildActions(context),
+    );
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: _buildActions(context),
+    );
+  }
+
+  List<Widget> _buildActions(BuildContext context) {
+    final actions = <Widget>[];
+    if (cancelActionText != null) {
+      actions.add(PlatformAlertDialogAction(
+        onPressed: () => Navigator.of(context).pop(false),
+        child: Text(cancelActionText),
+      ));
+    }
+    actions.add(
+      PlatformAlertDialogAction(
+          onPressed: () => Navigator.of(context).pop(true),
+          child: Text(defaultActionText)),
+    );
+    return actions;
+  }
+}
+
+class PlatformAlertDialogAction extends PlatformWidget {
+  final Widget child;
+  final VoidCallback onPressed;
+
+  PlatformAlertDialogAction({this.onPressed, this.child});
+
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return CupertinoDialogAction(
+      child: child,
+      onPressed: onPressed,
+    );
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return FlatButton(
+      child: child,
+      onPressed: onPressed,
+    );
+  }
+}
